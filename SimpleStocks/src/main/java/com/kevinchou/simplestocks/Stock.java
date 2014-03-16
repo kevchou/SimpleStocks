5742c6095a2e65356646ed2f0a83bb28c4306d63
Kevin Chou <kevchou@gmail.com>
2014-03-16 17:33:42 -0400

    version before publishing to play

---
+++ SimpleStocks/src/main/java/com/kevinchou/simplestocks/Stock.java
@@ -0,0 +1,259 @@
+package com.kevinchou.simplestocks;
+
+import android.graphics.drawable.Drawable;
+
+public class Stock
+{
+    public Stock() {}
+
+    private String prevClose;
+    private String open;
+    private String bid;
+    private String ask;
+    private String oneYearTarget;
+    private String price;
+    private String priceChange;
+    private String marketCap;
+    private String trailingPE;
+    private String forwardPE;
+    private String pegRatio;
+    private String priceSales;
+    private String priceBook;
+    private String ebitda;
+    private String tradeDate;
+
+    public String getDaysRange() {
+        return daysRange;
+    }
+
+    public void setDaysRange(String daysRange) {
+        this.daysRange = daysRange;
+    }
+
+    public String getYearRange() {
+        return yearRange;
+    }
+
+    public void setYearRange(String yearRange) {
+        this.yearRange = yearRange;
+    }
+
+    private String daysRange;
+    private String yearRange;
+
+
+    public String getAvgDailyVolume() {
+        return avgDailyVolume;
+    }
+
+    public void setAvgDailyVolume(String avgDailyVolume) {
+        this.avgDailyVolume = avgDailyVolume;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    private String avgDailyVolume;
+    private String volume;
+
+    private String tradeTime;
+    private String name;
+    private String ticker;
+    private String exchange;
+
+
+    private Drawable priceChart;
+
+    public String getTradeTime() {
+        return tradeTime;
+    }
+
+    public void setTradeTime(String tradeTime) {
+        this.tradeTime = tradeTime;
+    }
+
+    // Handles the price change info
+    public int getPriceChangeDirection() {
+
+        if(priceChange.length() > 0) {
+
+            String d = priceChange.substring(0, 1);
+
+            if (d.equals("-"))
+            {
+                return -1;
+            }
+            else if (d.equals("+"))
+            {
+                return 1;
+            }
+
+        }
+        return 0;
+
+    }
+
+
+
+    // Getters and setters
+
+    public Drawable getPriceChart() {
+        return priceChart;
+    }
+
+    public void setPriceChart(Drawable priceChart) {
+        this.priceChart = priceChart;
+    }
+
+    public String getPrevClose() {
+        return prevClose;
+    }
+
+    public void setPrevClose(String prevClose) {
+        this.prevClose = prevClose;
+    }
+
+    public String getOpen() {
+        return open;
+    }
+
+    public void setOpen(String open) {
+        this.open = open;
+    }
+
+    public String getBid() {
+        return bid;
+    }
+
+    public void setBid(String bid) {
+        this.bid = bid;
+    }
+
+    public String getAsk() {
+        return ask;
+    }
+
+    public void setAsk(String ask) {
+        this.ask = ask;
+    }
+
+    public String getOneYearTarget() {
+        return oneYearTarget;
+    }
+
+    public void setOneYearTarget(String oneYearTarget) {
+        this.oneYearTarget = oneYearTarget;
+    }
+
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPriceChange() {
+        return priceChange;
+    }
+
+    public void setPriceChange(String priceChange) {
+        this.priceChange = priceChange;
+    }
+
+    public String getMarketCap() {
+        return marketCap;
+    }
+
+    public void setMarketCap(String marketCap) {
+        this.marketCap = marketCap;
+    }
+
+    public String getTrailingPE() {
+        return trailingPE;
+    }
+
+    public void setTrailingPE(String trailingPE) {
+        this.trailingPE = trailingPE;
+    }
+
+    public String getForwardPE() {
+        return forwardPE;
+    }
+
+    public void setForwardPE(String forwardPE) {
+        this.forwardPE = forwardPE;
+    }
+
+    public String getPegRatio() {
+        return pegRatio;
+    }
+
+    public void setPegRatio(String pegRatio) {
+        this.pegRatio = pegRatio;
+    }
+
+    public String getPriceSales() {
+        return priceSales;
+    }
+
+    public void setPriceSales(String priceSales) {
+        this.priceSales = priceSales;
+    }
+
+    public String getPriceBook() {
+        return priceBook;
+    }
+
+    public void setPriceBook(String priceBook) {
+        this.priceBook = priceBook;
+    }
+
+    public String getEbitda() {
+        return ebitda;
+    }
+
+    public void setEbitda(String ebitda) {
+        this.ebitda = ebitda;
+    }
+
+    public String getTradeDate() {
+        return tradeDate;
+    }
+
+    public void setTradeDate(String tradeDate) {
+        this.tradeDate = tradeDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public String getExchange() {
+    return exchange;
+}
+
+    public void setExchange(String exchange) {
+        this.exchange = exchange;
+    }
+
+
+}
---
+++ SimpleStocks/src/main/java/com/kevinchou/simplestocks/YahooFinanceInfo.java
@@ -0,0 +1,149 @@
+package com.kevinchou.simplestocks;
+
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class YahooFinanceInfo {
+
+    // Yahoo finance symbols
+    final static String yPrice           = "l1";
+    final static String yChange          = "c";
+    final static String yPrevClose       = "p";
+    final static String yOpen            = "o";
+    final static String yBid             = "b3";
+    final static String yAsk             = "b2";
+    final static String yOneYrTarget     = "t8";
+    final static String yMarketCap       = "j1";
+    final static String yTrailingPE      = "r";
+    final static String yForwardPE       = "r7";
+    final static String yPegRatio        = "r5";
+    final static String yPriceSales      = "p5";
+    final static String yPriceBook       = "p6";
+    final static String yEbitda          = "j4";
+    final static String yTradeDate       = "d1";
+    final static String yTradeTime       = "t1";
+    final static String yExchange        = "x";
+    final static String yName            = "n";
+    final static String yDaysRange       = "m0";
+    final static String yYearRange       = "w0";
+    final static String yAvgDailyVolume  = "a2";
+    final static String yVolume          = "v0";
+
+
+
+    public static Stock getStockData(String ticker) {
+
+        String[] companyData;
+
+        Stock stock = new Stock();
+        stock.setTicker(ticker);
+
+        try {
+
+
+            URL url = new URL("http://finance.yahoo.com/d/quotes.csv?s="
+                    + ticker
+                    + "&f="
+                    + yPrice
+                    + yChange
+                    + yPrevClose
+                    + yOpen
+                    + yBid
+                    + yAsk
+                    + yOneYrTarget
+                    + yMarketCap
+                    + yTrailingPE
+                    + yForwardPE
+                    + yPegRatio
+                    + yPriceSales
+                    + yPriceBook
+                    + yEbitda
+                    + yTradeDate
+                    + yTradeTime
+                    + yExchange
+                    + yAvgDailyVolume
+                    + yVolume
+                    + yDaysRange
+                    + yYearRange
+                    + yName
+            );
+            URLConnection yc = url.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+
+// Split CSV by comma and quotes
+            companyData = in.readLine().split(",");
+            in.close();
+
+// Convert price string to float, then round to 2 digits.
+            stock.setPrice(String.format("$%.2f", Float.parseFloat(companyData[0])));
+
+// Separate actual change and percent change
+            String[] priceChange = companyData[1].replaceAll("\"", "").split(" - ");
+            stock.setPriceChange(priceChange[0] + " (" + priceChange[1] + ")");
+
+            stock.setPrevClose(companyData[2]);
+            stock.setOpen(companyData[3]);
+            stock.setBid(companyData[4]);
+            stock.setAsk(companyData[5]);
+            stock.setOneYearTarget(companyData[6]);
+            stock.setMarketCap(companyData[7]);
+            stock.setTrailingPE(companyData[8]);
+            stock.setForwardPE(companyData[9]);
+            stock.setPegRatio(companyData[10]);
+            stock.setPriceSales(companyData[11]);
+            stock.setPriceBook(companyData[12]);
+            stock.setEbitda(companyData[13]);
+            stock.setTradeDate(companyData[14].replaceAll("\"", ""));
+            stock.setTradeTime(companyData[15].replaceAll("\"", ""));
+            stock.setExchange(companyData[16].replaceAll("\"", ""));
+            stock.setAvgDailyVolume(companyData[17]);
+            stock.setVolume(companyData[18]);
+            stock.setDaysRange(companyData[19].replaceAll("\"", ""));
+            stock.setYearRange(companyData[20].replaceAll("\"", ""));
+
+            // Name is always last, since name can contain a comma.
+            stock.setName(companyData[companyData.length-1].replaceAll("\"", ""));
+
+        } catch (Exception e) {
+
+            Log.d("ERROR_GETTINGDATA", e.toString());
+
+        }
+
+        return stock;
+    }
+
+    // This method will retrieve the price chart from Yahoo and returns it as a drawable
+    public static Drawable getPriceChart(String ticker, String timeInterval, String plotType) {
+
+        Drawable d = null;
+
+        try {
+
+            URL url = new URL("http://chart.finance.yahoo.com/z?"
+                    + "s=" + ticker
+                    + "&t=" + timeInterval  // Time interval, {1d, 5d, 3m, 6m, 1y, 2y, 5y, my}
+                    + "&q=" + plotType      // Chart type {l, b, c}
+                    + "&l=" + "on"  // Logarithmic scaling {on, off}
+                    + "&z=" + "m"   // Size {m, l}
+                    + "&a=v" // Volume
+                    );
+
+            InputStream content = (InputStream)url.getContent();
+            d = Drawable.createFromStream(content, "src");
+        } catch (Exception e) {
+
+            Log.d("ERROR_GETTINGCHARTS", e.toString());
+
+        }
+
+        return d;
+    }
+
+}
